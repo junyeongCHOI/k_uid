41521f74d955b9dd115a039e3a6c057d5fe97ce3
최준영 <98.junyeong@gmail.com>
2024-05-01 20:37:46 +0900

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# k_uid
+
+10자리 한글 고유 아이디.
+초당 100만 개 생성 시 중복 확률 50%에 도달하는 시점은 은 약 650만 년 후
+
+## 설치
+
+```bash
+npm install k_uid
+```
+
+## 사용
+
+```js
+import kuid from "k_uid";
+
+const id = kuid(); // "까먀똞느닒댜맒님머먘"
+const id2 = kuid("user_", 8); // "user_까니뇨또냬먀님"
+```
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+function k_uid(prefix = "", length = 10): string {
+  const a = 0xac00,
+    z = 0xd7a3,
+    r = a - z + 1;
+  let result = prefix;
+
+  const gen =
+    typeof crypto !== "undefined"
+      ? (): void => {
+          const buffer = new Uint32Array(1);
+          crypto.getRandomValues(buffer);
+          result += String.fromCharCode(a + (buffer[0] % r));
+        }
+      : (): void => {
+          result += String.fromCharCode(a + Math.floor(Math.random() * r));
+        };
+
+  for (let i = 0; i < length; i++) gen();
+
+  return result;
+}
+
+// @ts-ignore
+if (typeof window !== "undefined") window.k_uid = k_uid;
+
+export default k_uid;
